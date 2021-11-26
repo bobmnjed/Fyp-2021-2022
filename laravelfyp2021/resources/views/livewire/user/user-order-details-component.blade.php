42c7bd58cb6d85e8523ce18a6819ec77b3be71f6
bobmnjed <60136539+bobmnjed@users.noreply.github.com>
2021-11-26 18:30:22 +0200

    cancel order for user and review

--- laravelfyp2021/resources/views/livewire/user/user-order-details-component.blade.php
+++ laravelfyp2021/resources/views/livewire/user/user-order-details-component.blade.php
@@ -2,16 +2,48 @@
     <div class="container" style="padding: 30px 0;">
         <div class="row">
             <div class="col-md-12">
+                @if(Session::has('order_message'))
+                    <div class="alert alert-success" role="alert">{{Session::get('order_message')}}</div>
+                @endif
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <div class="row">
                             <div class="col-md-6">
-                                <h3>Ordered Items</h3>
+                                <h3>Order Details</h3>
                             </div>
                             <div class="col-md-6">
                                 <a href="{{route('user.orders')}}" class="btn btn-success pull-right">All Orders</a>
+                                @if($order->status == 'ordered')
+                                <a href="#" class="btn btn-warning pull-right" style="margin-right: 4px" wire:click.prevent="cancelOrder">Cancel Order</a>
+                                @endif
                             </div>
                         </div>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table">
+                            <th>Order Id</th>
+                            <td>{{$order->id}}</td>
+                            <th>Order Date</th>
+                            <td>{{$order->created_at}}</td>
+                            <th>Status</th>
+                            <td style="color: black">{{$order->status}}</td>
+                            @if ($order->status == 'delivered')
+                            <th>Delivery Date</th>
+                            <td style="color: black">{{$order->delivered_date}}</td>
+                            @elseif($order->status == 'canceled')
+                            <th>Cancellation Date</th>
+                            <td style="color: black">{{$order->canceled_date}}</td>
+                            @endif
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3>Ordered Items</h3>
                     </div>
                     <div class="panel-body">
                         <div class="wrap-iten-in-cart">
@@ -29,7 +61,10 @@
                                     <div class="quantity">
                                          <h5>{{$item->quantity}}</h5>
                                     </div>
-                                    <div class="price-field sub-total"><p class="price">${{$item->price * $item->quantity}}</p></div>                                 
+                                    <div class="price-field sub-total"><p class="price">${{$item->price * $item->quantity}}</p></div> 
+                                    @if($order->status == 'delivered' && $item->rstatus == false) 
+                                    <div class="price-field sub-total"><p class="price"><a href="{{route('user.review',['order_item_id'=>$item->id])}}">Write Review</a></p></div>  
+                                    @endif
                                 </li>
                                 @endforeach                                                
                             </ul>                           
