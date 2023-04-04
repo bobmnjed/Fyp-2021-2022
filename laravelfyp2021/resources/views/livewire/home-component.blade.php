2063800ca451b1036fe148535f8140245319727b
bobmnjed <60136539+bobmnjed@users.noreply.github.com>
2023-04-04 10:08:04 +0300

    Hi

--- laravelfyp2021/resources/views/livewire/home-component.blade.php
+++ laravelfyp2021/resources/views/livewire/home-component.blade.php
@@ -1,4 +1,3 @@
-
 </header>
 <!-- end header -->
 <section class="slider_section">
@@ -13,14 +12,14 @@
         <p>It is a long established fact that a reader will be distracted by the readable content
             <br> of a page when looking at its layout. The point of using Lorem Ipsum is that</p>
         <a class="buynow" href="#">Buy Now</a>
-        <ul class="social_icon">
+       <!-- <ul class="social_icon">
             <li><a href="{{$setting->twitter}}" class="link-to-item" title="twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
             <li><a href="{{$setting->facebook}}" class="link-to-item" title="facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
             <li><a href="{{$setting->pinterest}}" class="link-to-item" title="pinterest"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
             <li><a href="{{$setting->instagram}}" class="link-to-item" title="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
             <li><a href="{{$setting->youtube}}" class="link-to-item" title="youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
 
-        </ul>
+        </ul>-->
     </div>
 </div>
 </div>
